4dd485bab07b857aa232ebed4fbe7740b28c43ba
Jasmin Husić <jhsc1988@gmail.com>
2021-02-05 20:49:18 +0100

    Add project files.

---
+++ sqlupiti.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SQLupiti
+{
+    public partial class sqlupiti : Form
+    {
+        public sqlupiti()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
